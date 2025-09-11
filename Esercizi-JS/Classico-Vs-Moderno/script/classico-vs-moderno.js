45e3e2ba9191253c36d0903a1793bbfcea5e7d88
Vincenzo Crucitti <vincenzo.crucitti04@gmail.com>
2025-09-11 15:24:54 +0200

    feat: Add calculator and shopping list functionalities with dynamic product catalog

---
+++ Esercizi-JS/Classico-Vs-Moderno/script/classico-vs-moderno.js
@@ -0,0 +1,60 @@
+// studenti di esempio
+const studenti = [
+  { nome: "Mario Rossi", voto: 8, presente: true },
+  { nome: "Luigi Bianchi", voto: 5, presente: false },
+  { nome: "Anna Verdi", voto: 9, presente: true },
+  { nome: "Beatrice Neri", voto: 6, presente: true },
+  { nome: "Carlo Blu", voto: 4, presente: true },
+  { nome: "Davide Rosa", voto: 7, presente: false },
+  { nome: "Elena Gialli", voto: 10, presente: true },
+  { nome: "Federico Viola", voto: 3, presente: false },
+  { nome: "Giorgia Azzurra", voto: 8, presente: true },
+  { nome: "Lorenzo Marrone", voto: 2, presente: false },
+  { nome: "Martina Rosa", voto: 6, presente: true },
+  { nome: "Simone Nero", voto: 5, presente: true },
+];
+const btn = document.getElementById("run");
+const out = document.getElementById("output");
+
+// funzione per mostrare output
+function log(titolo, dato) {
+  const h4 = document.createElement("h4");
+  const pre = document.createElement("pre");
+  h4.textContent = titolo;
+  pre.textContent = JSON.stringify(dato, null, 2);
+  out.append(h4, pre);
+}
+btn.addEventListener("click", () => {
+  out.innerHTML = ""; // reset output
+
+  //Operatore Ternario
+
+  const primoAlunno = studenti[0];
+  const Promosso =
+    primoAlunno.voto >= 6 && primoAlunno.presente === true
+      ? "Promosso"
+      : "Bocciato";
+  log("Es n 1", `${primoAlunno.nome} è stato ${Promosso}`);
+
+  //Spread
+
+  const alunniN = [
+    { nome: "Vincenzo Crucitti", voto: 8, presente: true },
+    { nome: "Arianna De Lucia", voto: 5, presente: false },
+  ];
+  const tutti = [...studenti, ...alunniN];
+  log("Es n 2", tutti);
+
+  //Rest
+
+  function raggruppaNomi(...nomi) {
+    return nomi;
+  }
+  const gruppo1 = raggruppaNomi("Mario", "Luigi", "Anna");
+  log("Es n 3 Raggruppa Nomi (con 3 nomi)", gruppo1);
+
+  // Map()
+
+  const arrayTrasformato = tutti.map((el) => `${el.nome}: ${el.voto}`);
+  log("Es n 4 Map", arrayTrasformato);
+});
